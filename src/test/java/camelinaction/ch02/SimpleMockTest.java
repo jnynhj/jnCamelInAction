832f6958f2163008880b4a360e86ede7abef823c
Jinnan Ning <jnynhj@yahoo.com.au>
2022-05-31 22:49:26 +1000

    v1.0 SimpleMockTest and RouteTest run successfully 1st time

--- src/test/java/camelinaction/ch02/SimpleMockTest.java
+++ src/test/java/camelinaction/ch02/SimpleMockTest.java
@@ -1,15 +1,34 @@
 package camelinaction.ch02;
 
-import org.apache.camel.RoutesBuilder;
+
+import org.apache.camel.CamelContext;
+// import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimpleMockTest extends CamelTestSupport {
 
+    /*
+    @BeforeEach
+    public void setUp() throws Exception {
+        //start camel
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(createRouteBuilder());
+        context.start();
+        template = context.createProducerTemplate();
+    }
+*/
+
     @Test
-    public void testMock() throws Exception {
-        getMockEndpoint("mock:result").expectedBodiesReceived("Hello World");
+    void testMock() throws Exception {
+        MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
+        resultEndpoint.expectedBodiesReceived("Hello World");
+
+//        getMockEndpoint("mock:result").expectedBodiesReceived("Hello World");
 
         template.sendBody("direct:start", "Hello World");
 
@@ -17,7 +36,7 @@ public class SimpleMockTest extends CamelTestSupport {
     }
 
     @Override
-    protected RoutesBuilder createRouteBuilder() {
+    protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             @Override
             public void configure() {
@@ -25,5 +44,5 @@ public class SimpleMockTest extends CamelTestSupport {
             }
         };
     }
-
 }
+
